317c008f6d4d46e37b1faa69e06d714f75ea1534
Karim Ben Romdhane <karoumbr@gmail.com>
2020-11-22 09:27:45 +0100

    initial commit

---
+++ app/src/main/java/com/bentechprotv/android/tp02laclasseintent/ComActActivity.java
@@ -0,0 +1,14 @@
+package com.bentechprotv.android.tp02laclasseintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComActActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com_act);
+    }
+}
---
+++ app/src/main/java/com/bentechprotv/android/tp02laclasseintent/ComAppActivity.java
@@ -0,0 +1,14 @@
+package com.bentechprotv.android.tp02laclasseintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com_app);
+    }
+}
